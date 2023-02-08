c9705d5adb45e8825fa4c5b5474869c33a1b17d6
Gokulkrish1010 <gokulkrishnav10@gmail.com>
2023-02-08 11:54:42 +0530

    s

--- src/test/java/org/utilities/Basic.java
+++ src/test/java/org/utilities/Basic.java
@@ -3,10 +3,7 @@ package org.utilities;
 public class Basic {
 	public static void main(String[] args) {
 		System.out.println("done");
-<<<<<<< HEAD
-		System.out.println("gokul");
-=======
 		System.out.println("krishna");
->>>>>>> 78e22630b873bc9696481e823865656a26186940
+		System.out.println("gokul");
 	}
 }
